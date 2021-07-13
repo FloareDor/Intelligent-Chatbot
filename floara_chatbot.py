06b39026d1d6033cb9efeac4907c51175307b72c
Sai Ravi Teja G <73626236+FloareDor@users.noreply.github.com>
2021-07-14 03:29:58 +0530

    Implemented a multi-language system.
    the bot can now understand a lot of languages.

--- floara_chatbot.py
+++ floara_chatbot.py
@@ -7,6 +7,30 @@ from nltk.stem import WordNetLemmatizer
 import tensorflow
 import discord
 from discord.ext import commands
+from asyncio.tasks import wait
+from asyncio.windows_events import NULL
+import discord
+from discord.ext import commands
+import praw
+import random as rand
+import asyncio
+import speech_recognition as sr
+import pyttsx3
+import webbrowser
+import requests as req
+from youtubesearchpython import VideosSearch
+from bs4 import BeautifulSoup
+from pynput.keyboard import Controller,Key
+from googletrans import Translator
+from gtts import gTTS
+import os
+html_text = req.get("https://developers.google.com/admin-sdk/directory/v1/languages").text
+soup = BeautifulSoup(html_text, 'lxml')
+langs = soup.findAll('td')
+lang_Codes = []
+for lang in langs:
+    lang_Codes.append(lang.text.lower())
+trans = Translator()
 physical_devices = tensorflow.config.list_physical_devices('GPU') 
 tensorflow.config.experimental.set_memory_growth(physical_devices[0], True)
 from tensorflow.keras.models import load_model
@@ -45,28 +69,44 @@ def predict_class(sentence):
     return return_list
 
 def get_response(intents_list, intents_json):
-    result = "hi"
+    ans = "I don't think I understand that"
     tag = intents_list[0]['intent']
     #print(tag)
     list_of_intents = intents_json['intents']
     for i in list_of_intents:
         if i['tag'] == tag:
             result = random.choice(i['responses'])
-            return result
+            print(float(intents_list[0]['probability']))
+            if float(intents_list[0]['probability']) > 0.65:
+                ans = result
+    return ans    
 print("Floara is running : )")
-client = commands.Bot(command_prefix = "^")
+#while True:
+#   temp = input()
+#   message = str(temp)
+#   ints = predict_class(message)
+#   res = get_response(ints, intents)
+#   print(res)
+
+client = commands.Bot(command_prefix = "f")
 
 @client.event
-async def on_ready(ctx):
+async def on_ready():
     print("Bot is ready af")
-    await ctx.send("type `help me` for commands :)")
+    #await ctx.send("type `help me` for commands :)")
 @client.command()
-async def f(ctx,*,arg):
-    message = str(arg)
+async def l(ctx,*,arg):
+    temp = str(arg)
+    src_code = trans.detect(temp).lang
+    # print(src_code)
+    if src_code != 'en':
+        temp = trans.translate(temp, src = src_code, dest = 'en').text
+    print(temp)
+    message = str(temp)
     ints = predict_class(message)
     res = get_response(ints, intents)
     await ctx.send(res)
-client.run("ToKeN")
+client.run("tOkEn")
 
     
 
